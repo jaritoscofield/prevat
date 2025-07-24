2307f7c016722d11c5708eb2281a6d9e85b7ead9
Jayro Anderson <jayrosystem@gmail.com>
2025-07-24 16:03:21 -0300

    ajuste no conteudo programatico

--- resources/views/admin/pdf/evidence_certificates_03.blade.php
+++ resources/views/admin/pdf/evidence_certificates_03.blade.php
@@ -15,7 +15,7 @@
 
 <div>
     {{-- Gera o ATESTADO apenas uma vez --}}
-    <div class="web-container">
+    <div class="web-container">ajus
         <table class="line-items-container">
             <thead>
             <tr>
@@ -82,7 +82,38 @@
             </table>
         </div>
     </div>
-    <div class="page-break"></div>
+
+    {{-- CONTEÚDO PROGRAMÁTICO logo após o atestado --}}
+    <div class="web-container" style="margin-top: 40px;">
+        <div style="text-align: center; font-size: 20px; font-weight: bold; margin-bottom: 10px; text-transform: uppercase; letter-spacing: 1px;">
+            CONTEÚDO PROGRAMÁTICO<br>
+            <span style="font-size: 16px; font-weight: normal;">Aulas teóricas (16h):</span>
+        </div>
+        <ol type="1" style="font-size: 15px; line-height:2; margin: 0 auto; max-width: 700px; padding-left: 30px;">
+            <li>Introdução (Objetivos do curso e dos Brigadistas);</li>
+            <li>O que é fogo;</li>
+            <li>Triângulo do fogo;</li>
+            <li>Teoria do fogo (Combustão, seus elementos e a reação em cadeia);</li>
+            <li>Propagação do fogo; Classes de incêndio;</li>
+            <li>Métodos de extinção;</li>
+            <li>Agentes extintores;</li>
+            <li>Extintores de incêndio;</li>
+            <li>Técnicas de combate a incêndio com extintores;</li>
+            <li>Procedimentos básicos em locais de Incêndio;</li>
+            <li>Sistemas fixos de combate a incêndio;</li>
+            <li>Sistemas de detecção, alarme e comunicações;</li>
+            <li>Ferramentas de salvamento;</li>
+            <li>Técnicas de combate a incêndio com uso de mangueiras e hidrantes;</li>
+            <li>Exigências legais quanto à instalação, localização e sinalização dos extintores de incêndio e hidrantes;</li>
+            <li>Pessoas com mobilidade reduzida;</li>
+            <li>Procedimentos básicos em locais de Incêndio;</li>
+            <li>Riscos específicos da planta e Prevenção de incêndio;</li>
+            <li>Plano de Emergência;</li>
+            <li>Procedimentos para abandono de área e controle de pânico.</li>
+            <li>Aulas teóricas (08h)</li>
+            <li>Práticas (08h)</li>
+        </ol>
+    </div>
 
     {{-- Gera um CERTIFICADO para cada participante --}}
     @foreach($certifications as $itemCertification)
@@ -166,236 +197,6 @@
                         </td>
                     </tr>
                 </table>
-            </div>
-        </div>
-        <div class="page-break"></div>
-        {{-- O restante do conteúdo do certificado (objetivo, conteúdo programático, etc.) pode ser mantido aqui, se for individual por participante. --}}
-        <div class="web-container">
-            <div class="content03">
-                <p> <span class="bold">OBJETIVO:</span> Proporcionar aos participantes, formação e conhecimentos técnicos na área de combate a incêndio e outras emergências tecnológicas,
-                    em áreas sinistradas, dispondo de todos os equipamentos e materiais necessários para um bom desempenho nas atividades de salvar vidas e preservar
-                    o bem patrimonial da empresa
-                </p>
-                <br>
-            </div>
-            <div class="content-programatic_03">
-                <table style="font-size: 12px; line-height:1.5em; border-spacing: 15px; font-family: Arial; ">
-                    <tr>
-                        <th width="65%" style="text-align: left; vertical-align:top; padding-right: 30px; ">
-                            <div class="content-programatic-title-02 center">
-                                <p class="">CONTEÚDO PROGRAMÁTICO PARA TREINAMENTO DE BRIGADA DE INCÊNDIO </p>
-                            </div>
-                            <table style="border: 1px solid;  border-collapse: collapse;">
-                                <tr>
-                                    <th width="50%" style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        MODULO
-                                    </th>
-
-                                    <th width="50%" style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        ASSUNTO
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        INTRODUÇÃO
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Objetivos do curso e Brigada de incêndio
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        ASPECTOSLEGAIS.
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Responsabilidade do brigadista
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        TEORIA DO FOGO
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Combustão, seus elementos e a reação em cadeia.
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        PROPAGAÇÃO DO FOGO
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Condução, irradiação e convecção.
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        CLASSES DE INCÊNDIO
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Classificação e características
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        PREVENÇÃO DE INCÊNDIOS
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Técnicas de prevenção.
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        MÉTODOS DE EXTINÇÃO
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Isolamento, abafamento, resfriamento e extinção química
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        INTRODUÇÃO
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Objetivos do curso e Brigada de incêndio
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        AGENTES EXTINTORES
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Água, Pós, CO2, Espumas e outros.
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        EPI (EQUIPAMENTOS DE PROTEÇÃO
-                                        INDIVIDUA
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        EPI.
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        EQUIPAMENTOS DE COMBATE A INCÊNDIO
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Extintores e acessórios
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        EQUIPAMENTOS DE COMBATEA INCÊNDIO
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Hidrantes, mangueiras e acessórios
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        EQUIPAMENTOS DE DETECÇÃO,
-                                        ALARME, LUZ DE EMERGÊNCIA E
-                                        COMUNICAÇÕES
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Tipos e funcionamento.
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        ABANDONO DE ÁREA
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Conceitos.
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        AVALIAÇÃO INICIAL
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Avaliação do cenário, mecanismo de lesão
-                                        e número de vítimas.
-
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        VIAS AÉREAS
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Causas de obstrução e liberação
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        RCP (REANIMAÇÃO CARDIOPULMONAR)
-                                    </th>
-                                    <th  style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Ventilação artificial e compressão cardíaca externa.
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding-right: 10px; border: 1px solid;  border-collapse: collapse;">
-                                        HEMORRAGIAS
-                                    </th>
-                                    <th style="text-align: center; vertical-align:top; border: 1px solid;  border-collapse: collapse; ">
-                                        Classificação e tratamento.
-                                    </th>
-                                </tr>
-
-                            </table>
-
-                        </th>
-                        <th width="35%" style="text-align: left; vertical-align:top;  ">
-                            <table style="margin-top:20px; border: 1px solid;  border-collapse: collapse;">
-                                <tr>
-                                    <th  style="text-align: center; font-style: italic; vertical-align:top; padding: 20px; border: 1px solid;  border-collapse: collapse;">
-                                        CERTIFICADO DE EDUCAÇÃO PROFISSIONAL DE NÍVEL
-                                        BÁSICO ESTA REGULAMENTADO PELO DECRETO 2.208, DE
-                                        17 DE ABRIL DE 1997, DO MINISTÉRIO DE EDUCAÇÃO E LEI
-                                        N° 6.514 DE 22 DE DEZEMBRO DE 1977, CAPITULO V DA
-                                        SEGURANÇA E MEDICINA DO TRABALHO.
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th  style="text-align: center; vertical-align:top; padding: 20px; border: 1px solid;  border-collapse: collapse;">
-                                        REGISTRO ESPECIFICAÇÃO
-                                        TREINAMENTO:FORMAÇÃO BRIGADA DE INCÊNDIOCFBI
-                                        CARGA HORÁRIA: 16 HORAS.NÍVELBÁSICO
-                                        REFERENCIANORMATIVA: PORTARIA Nº3.214/1978,NR
-                                        23
-                                        - PROTEÇÃO CONTRA INCÊNDIOS, NBR 14276 E IT 08
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th style="text-align: center; vertical-align:top; font-size:14px; padding: 20px; border: 1px solid;  border-collapse: collapse;">
-                                <span style="color:red;">
-                                    Certificado de Licenciamento<br>
-                                    Nº {{ $licenca_numero ?? '' }}<br>
-                                    Validade: {{ !empty($licenca_validade) ? \Carbon\Carbon::parse($licenca_validade)->format('d/m/Y') : '' }}<br>
-                                    Nº do Protocolo: {{ $licenca_protocolo ?? '' }}
-                                </span>
-                                    </th>
-                                </tr>
-                            </table>
-                            <table class="">
-                                <tr>
-                                    <th style="text-align: center; vertical-align:top; font-size:14px; color:red; padding: 20px;" >
-                                        @foreach($professionals['professionals'] as $itemProfessional)
-                                            @if($itemProfessional['verse'] && $itemProfessional['professional']['signature_image'])
-                                                <img  style="height: 120px; padding-bottom:25px; padding-left: 20px; padding-right: 30px;" src="{{ url('images/signatures/' . $itemProfessional['professional']['signature_image']) }}" alt="assinatura">
-                                            @endif
-                                        @endforeach
-
-
-                                    </th>
-                                </tr>
-                            </table>
-
-                </table>
-
             </div>
         </div>
         @if(!$loop->last)
